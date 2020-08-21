d19f1e9ae2e883213f397254535edac067b780c6
Артём Чернышов <inviolablejade@gmail.com>
2020-08-22 00:35:47 +0300

    Initialization of my web app

---
+++ TestProjs/TestProj1/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TestProj1
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                options.HttpsPort = 44344;
+            });
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(60);
+                options.ExcludedHosts.Add("us.example.com");
+                options.ExcludedHosts.Add("www.example.com");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            env.EnvironmentName = "Production";
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+                app.UseExceptionHandler("/error");
+            }
+            app.UseStatusCodePagesWithReExecute("/error", "?code={0}");
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseHttpsRedirection();
+            app.Map("/error", app => app.Run(async context =>
+             {
+                 await context.Response.WriteAsync("DBZ error happened");
+             }));
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    int x = 0;
+                    int y = 8 / x;
+                    await context.Response.WriteAsync($"Result = {y}");
+                });
+            });
+        }
+    }
+}
